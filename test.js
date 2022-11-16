80a093eae390765b9fd0c519b66d5780af545c90
zzcDesigner <zhicheng.zhu@aispeech.in>
2022-11-16 11:36:45 +0800

    update

---
+++ test.js
@@ -0,0 +1,32 @@
+console.log('script start');
+async function async1() {
+    await async2();
+    console.log('async1 end');
+};
+async function async2() {
+    console.log('async2 end');
+    return Promise.resolve().then(() => {
+        setTimeout(function () {
+            console.log('setTimeout2');
+        }, 0);
+        console.log('async2 end1');
+    });
+};
+
+async1();
+
+setTimeout(function () {
+    console.log('setTimeout');
+}, 0);
+
+new Promise(resolve => {
+    console.log('Promise');
+    resolve();
+})
+.then(function () {
+    console.log('promise1');
+})
+.then(function () {
+    console.log('promise2');
+});
+console.log('script end');
