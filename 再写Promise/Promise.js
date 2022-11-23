8ce042a673012bd1c9a8e9e36ace4c64a76ab4b4
zzcDesigner <zhicheng.zhu@aispeech.in>
2022-11-23 19:58:07 +0800

    update

--- 再写Promise/Promise.js
+++ 再写Promise/Promise.js
@@ -1,12 +1,45 @@
 function Promise(executor) {
+    this.PromiseState = 'pending'
+    this.PromiseResult = null
+    this.callbacks = []
+
+    const that = this
 
     function resolve(data) {
-
+        if(that.PromiseState !== 'pending') return 
+        that.PromiseState = 'fulfilled'
+        that.PromiseResult = data
+        that.callbacks.forEach((item) => {
+            item.onResolved(data)
+        })
     }
 
     function reject(data) {
-        
+        if(that.PromiseState !== 'pending') return 
+        that.PromiseState = 'rejected'
+        that.PromiseResult = data
+        that.callbacks.forEach((item) => {
+            item.onRejected(data)
+        })
     }
 
-    executor(resolve, reject)
+    try {
+        executor(resolve, reject)
+    } catch (error) {
+        reject(error)
+    }
+}
+
+Promise.prototype.then = function(onResolved, onRejected) {
+    if(this.PromiseState === 'fulfilled') {
+        onResolved(this.PromiseResult)
+    }
+
+    if(this.PromiseState === 'rejected') {
+        onRejected(this.PromiseResult)
+    }
+
+    if(this.PromiseState === 'pending') {
+        this.callbacks.push({onResolved, onRejected})
+    }
 }
